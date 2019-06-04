d7c52d1fb80d105abb387a5c5f932fa5a87cd7ae
Lourival Netto <lourival.netto@ccc.ufcg.edu.br>
2019-06-04 09:55:15 -0300

    use case

--- src/ECO/Facade.java
+++ src/ECO/Facade.java
@@ -4,28 +4,38 @@ package ECO;
 import easyaccept.EasyAccept;
 
 public class Facade {
-    private Controller controller;
+    private PessoaController controller;
 
-    public static void main(String[] args)
-    {
-        args = new String[]{"ECO.Facade","acceptance_test/use_case_1.txt","acceptance_test/use_case_2.txt","acceptance_test/use_case_3.txt","acceptance_test/use_case_4.txt"};
+    public Facade() {
+        this.controller = new PessoaController();
+    }
+
+    public static void main(String[] args) {
+        args = new String[]{"ECO.Facade", "Projeto_LP2/acceptance_test/use_case_1.txt", "Projeto_LP2/acceptance_test/use_case_2.txt", "Projeto_LP2/acceptance_test/use_case_3.txt", "Projeto_LP2/acceptance_test/use_case_4.txt"};
         EasyAccept.main(args);
     }
 
-    public void cadastrarPessoa(String nome, String dni, String estado, String interesses){
+    public void cadastrarPessoa(String nome, String dni, String estado, String interesses) {
         controller.cadastrarPessoa(nome, dni, estado, interesses);
     }
 
-    public void cadastrarPessoa(String nome, String dni, String estado, String interesses, String partido){
+    public void cadastrarPessoa(String nome, String dni, String estado, String interesses, String partido) {
         controller.cadastrarPessoa(nome, dni, estado, interesses, partido);
     }
 
-    public void cadastrarDeputado(String DNI, String dataDeInicio)
-    {
-        controller.cadastrarDeputado(DNI,dataDeInicio);
+    public void cadastrarDeputado(String DNI, String dataDeInicio) {
+        controller.cadastrarDeputado(DNI, dataDeInicio);
     }
 
-    public String exibirPessoa(String DNI){
+    public String exibirPessoa(String DNI) {
         return controller.exibirPessoa(DNI);
     }
+
+    public void limparSistema() {
+    }
+
+    public void salvarSistema() {
+    }
+
+    public void carregarSistema(){}
 }
