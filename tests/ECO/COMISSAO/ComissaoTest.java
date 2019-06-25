fc1f5925f76250bfe46c482d4e85016777ad6adc
Lourival Netto <lourival.netto@ccc.ufcg.edu.br>
2019-06-25 08:37:48 -0300

    AULA 25/06

---
+++ tests/ECO/COMISSAO/ComissaoTest.java
@@ -0,0 +1,24 @@
+package ECO.COMISSAO;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComissaoTest {
+
+    @Test
+    void getTema() {
+    }
+
+    @Test
+    void getTamanhoComissao() {
+    }
+
+    @Test
+    void interessesDeputados() {
+    }
+
+    @Test
+    void getDeputados() {
+    }
+}
